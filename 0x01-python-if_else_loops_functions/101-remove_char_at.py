99c069dc0f106e90447a815ecda43a51be3dfe98
KFredlund <katihomes@gmail.com>
2019-12-11 22:26:04 +0000

    Create a function that creates a copy of the string and removes a given character

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
-        print("{:s}".format(i), end="")
+    array = list(str)
+    for i in range(0, len(str) - 1):
+        if i >= n:
+            array[i] = array[i + 1]
+    result = "".join(array)
+    return result[:-1]
