d4c4c8b96e115066a0387b1c5c9ba1020ded22d7
KFredlund <katihomes@gmail.com>
2019-12-12 15:16:13 +0000

    Check for empty list and and insert new node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,14 +15,20 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node;
 	listint_t *current;
 
+	if (head == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = number;
 /*	new_node->next = NULL;   */
 	current = *head;
-	if (head == NULL)
-		return (NULL);
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		*head = new_node;
+		return (*head);
+	}
 	if (current->n >= new_node->n)
 	{
 		new_node->next = *head;
