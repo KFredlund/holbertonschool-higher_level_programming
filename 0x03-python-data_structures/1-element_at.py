9bda887dcb6f373f976eb94ae7e424d269fb9b13
KFredlund <katihomes@gmail.com>
2019-12-17 14:22:46 +0000

    Use a for loop to iterate through list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+i = 0
+
+
 def element_at(my_list, idx):
     if idx < 0:
         return None
     if idx > len(my_list):
         return None
-    return my_list.index(idx + 2)
+    for i in range(0, idx):
+        numb = i
+    return (my_list[numb + 1])
