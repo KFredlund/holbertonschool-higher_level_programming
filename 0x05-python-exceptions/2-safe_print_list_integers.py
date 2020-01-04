6003cef641a3477f931434bca953461594c16e9f
KFredlund <katihomes@gmail.com>
2020-01-04 19:45:29 +0000

    Add a count to keep track of number of list elements

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except TypeError:
-            print()
-            return i
-        except ValueError:
-            print()
-            return i
+            count = count + 1
+        except (TypeError, ValueError):
+            continue
     print()
-    return i
+    return count
