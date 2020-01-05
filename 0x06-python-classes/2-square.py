5fa16e1cfeee8517d54277d03a49054a0b197192
KFredlund <katihomes@gmail.com>
2020-01-05 07:23:07 +0000

    Create a function that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-    try:
-        sizeInt = isinstance(size, int)
-    except TypeError:
-        print("size must be an integer")
-    try:
-        sizeInt >= 0 is True
-    except ValueError:
-        print("size must be >= 0")
