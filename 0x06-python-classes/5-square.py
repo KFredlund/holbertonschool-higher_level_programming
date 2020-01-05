1764ea39a566dcfbdf243bab756e3761c880b3f0
KFredlund <katihomes@gmail.com>
2020-01-05 07:48:09 +0000

    Create a class that defines a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size is 0:
+            print()
+        for row in range(self.__size):
+            for col in range(self.__size):
+                print("#", end="")
+            print()
