04c022c2518e2b5f55d01a39ed0f4ae7e79220d5
KFredlund <katihomes@gmail.com>
2020-01-05 05:31:54 +0000

    Create a function that prints a value

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+def safe_print_integer_err(value):
+    from sys import stderr
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as er:
+        print("Exception: {}".format(er),  file=stderr)
+        return False
