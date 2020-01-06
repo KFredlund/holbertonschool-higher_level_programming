255798dce334ecbd230b83198d42d28cbaebff75
KFredlund <katihomes@gmail.com>
2020-01-06 22:18:41 +0000

    Write a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class MyList(list):
+    """
+    MyList is a class that inherits from list
+    """
+
+    def print_sorted(self):
+        """A public instance method that prints a sorted list"""
+        print(self)
