3de29bd9dacff80871f356f0678cb36435fc00d8
KFredlund <katihomes@gmail.com>
2020-01-24 08:48:38 +0000

    Update square with method dict

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,3 +46,9 @@ class Square(Rectangle):
             self.size = kwargs.get("size", self.size)
             self.x = kwargs.get("x", self.x)
             self.y = kwargs.get("y", self.y)
+
+    def to_dictionary(self):
+        """dict method"""
+        new_dict = ({'id': self.id, 'size': self.size, 'x':
+                    self.x, 'y': self.y})
+        return new_dict
