6c3f5b16834228239789dad5dbfad1544f7d7ecb
KFredlund <katihomes@gmail.com>
2020-01-24 07:54:55 +0000

    switch order of init statements

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,8 +7,8 @@ class Square(Rectangle):
     """Square class inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor init"""
-        super().__init__(size, size, x, y, id)
         self.__size = size
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """String method"""
