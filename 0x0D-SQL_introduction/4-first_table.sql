2aa43f4a883bd9df64a4b07234fffa5a439fc1e6
KFredlund <katihomes@gmail.com>
2020-02-06 20:46:52 +0000

    A script that creates a table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- A script that creates a tables called first_table
+-- in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL KEY AUTO_INCREMENT,
+name VARCHAR(256));
