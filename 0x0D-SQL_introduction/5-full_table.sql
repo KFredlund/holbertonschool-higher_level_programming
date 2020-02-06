3958fb3943db6eeaea641a96e68fea6797ebc666
KFredlund <katihomes@gmail.com>
2020-02-06 21:12:44 +0000

    Use SELECT

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,6 @@
 -- A script that prints the full description of the 
 -- table first_table from db hbtn_0c_0 in your server
-DESCRIBE first_table;
+SELECT *
+from information_schema.columns
+WHERE table_name = 'first_table'
+ORDER BY ordinal_position;
