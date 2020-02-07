67c011f4f0754c13fbe930e250e9b79a6dce8665
KFredlund <katihomes@gmail.com>
2020-02-07 06:34:17 +0000

    A script taht creates the database hbtn_0d_usa with tables states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+--A script that creates a db
+-- hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+id INT NOT NULL PRIMARY KEY AUTO INCREMENT UNIQUE,
+name VARCHAR(256) NOT NULL);
