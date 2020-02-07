eb727550090e6981cf98364a7e8453326f4b57af
KFredlund <katihomes@gmail.com>
2020-02-07 06:18:37 +0000

    Add guard for existing

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,6 @@
 -- A script that creates the db hbtn_0d_2
 -- and the user user_0d_2
-IF NOT EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'hbtn_0d_2')
-CREATE DATABASE hbtn_0d_2;
-DROP USER 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
-CREATE USER IF 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
-GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2. * TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
