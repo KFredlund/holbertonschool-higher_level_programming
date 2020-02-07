f9678e3751ab638373d915f5bc561f5f5fae69de
KFredlund <katihomes@gmail.com>
2020-02-07 06:12:06 +0000

    Remove drop

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,4 @@
 -- A script that creates the My SQL server
 -- user user_0d_1
-DROP USER 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
-
