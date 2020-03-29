4757b6c828ee75da8fb02af7d3be0cfc3ede9be8
KFredlund <katihomes@gmail.com>
2020-03-29 15:00:52 +0000

    Make parameters dynamic

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,13 +11,14 @@ def filter_table_states():
     """ A method that filters the states from a table """
     db = MySQLdb.connect(
         host="localhost",
-        user="root",
-        passwd="Bk08262012",
-        db="hbtn_0e_0_usa"
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-    states_list = cur.fetchmany(2)
+    cur.execute("SELECT id, name FROM states
+                WHERE name LIKE 'N%' ORDER BY states.id")
+    states_list = cur.fetchall()
     for x in states_list:
         print(x)
 
